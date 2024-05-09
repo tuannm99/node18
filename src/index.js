34a2a2a437a1d8d74d61a8475ed8da5154dd07c0
tuannm99 <tuannm@foobla.com>
2024-05-09 22:39:33 +0700

    init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+// const os = require('os')
+//
+// const cpus = os.cpus()
+// console.log(os.arch())
+// const async_hook = require('async_hooks')
+// const localStorage = new async_hook.AsyncLocalStorage()
+// localStorage.run('hihi', () => {})
+// localStorage.disable()
+// localStorage.getStore()
+// localStorage.enterWith()
+// localStorage.exit()
 
+setImmediate(() => {
+    console.log('❤❤❤ tuannm: [index.js][6][setImmediate]')
+})
+
+setTimeout(() => {
+    console.log('❤❤❤ tuannm: [index.js][6][setTimeout]')
+})
+
+// setInterval(() => {
+//     console.log('❤❤❤ tuannm: [index.js][14][setInterval]')
+// }, 1000)
+
+process.nextTick(() => {
+    console.log('❤❤❤ tuannm: [index.js][5][nextTick]')
+})
+// console.log('❤❤❤ tuannm: [index.js][3][cpus]', cpus)
